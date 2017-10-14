bdc3dfa1744569243a6000d4564941f6985e1528
edwin ismail <typhoon_ed@yahoo.com>
2017-10-14 18:40:03 +0200

    add a repo

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ stupid: "Buddy" });
-});
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientsecret: keys.googleClientSecret
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
