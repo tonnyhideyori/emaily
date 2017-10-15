1c7f1f39bb0f2ecbdc66fd9a766f754b008f7ee4
edwin ismail <typhoon_ed@yahoo.com>
2017-10-15 14:50:37 +0200

    add new folders

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
 module.exports = {
   googleClientID:
     "42567909106-dks624dalnu40ogvtum9khfpkgbsehf4.apps.googleusercontent.com",
-  googleClientSecret: "Cbp-11u5h3GvHdZVBkCw131N"
+  googleClientSecret: "Cbp-11u5h3GvHdZVBkCw131N",
+  mongoURI:
+    "mongodb://tonnyhideyori:edwayne112@ds119810.mlab.com:19810/emaily-dev"
 };
