e53dbe66f5fe6285ce0a00a4c19e4c9a4f54a589
edwin ismail <typhoon_ed@yahoo.com>
2017-10-16 08:37:17 +0200

    finish auth and mongoDB setups

--- config/keys.js
+++ config/keys.js
@@ -3,5 +3,6 @@ module.exports = {
     "42567909106-dks624dalnu40ogvtum9khfpkgbsehf4.apps.googleusercontent.com",
   googleClientSecret: "Cbp-11u5h3GvHdZVBkCw131N",
   mongoURI:
-    "mongodb://tonnyhideyori:edwayne112@ds119810.mlab.com:19810/emaily-dev"
+    "mongodb://tonnyhideyori:edwayne112@ds119810.mlab.com:19810/emaily-dev",
+  cookieKey: "ghjdjksjkjfhs,cnwchijqsjfkshfkjsf;s,fmlsfosjfjnsv;spvjsihjknk"
 };
--- services/passport.js
+++ services/passport.js
@@ -4,6 +4,15 @@ const mongoose = require("mongoose");
 const keys = require("../config/keys");
 const User = mongoose.model("users");
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -12,7 +21,17 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleid: profile.id }).save();
+      User.findOne({ googleid: profile.id }).then(existingUser => {
+        if (existingUser) {
+          //we have already a record with the profile
+          done(null, existingUser);
+        } else {
+          //we don't have already a record with the profile
+          new User({ googleid: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
