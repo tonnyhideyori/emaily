071f19ac45b8cab71fe4ac0b9e37e3c53769fe43
edwin ismail <typhoon_ed@yahoo.com>
2017-10-16 10:41:17 +0200

    finish auth flow

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,6 @@
-module.exports = {
-  googleClientID:
-    "42567909106-dks624dalnu40ogvtum9khfpkgbsehf4.apps.googleusercontent.com",
-  googleClientSecret: "Cbp-11u5h3GvHdZVBkCw131N",
-  mongoURI:
-    "mongodb://tonnyhideyori:edwayne112@ds119810.mlab.com:19810/emaily-dev",
-  cookieKey: "ghjdjksjkjfhs,cnwchijqsjfkshfkjsf;s,fmlsfosjfjnsv;spvjsihjknk"
-};
+if(process.env.NODE_ENV==='production'){
+module.exports=require('./prod')
+}
+elese{
+  module.exports=require('./dev')
+}
