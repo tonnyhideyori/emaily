9d073f14918badd00b5d6059d2f53d3c4dd36dd2
edwin ismail <typhoon_ed@yahoo.com>
2017-11-03 16:37:43 +0300

    Add billing and client side app

--- index.js
+++ index.js
@@ -25,16 +25,15 @@ require("./routes/billingRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
   // Express will serve up production assets
-  //file like main.js
+  // like our main.js file, or main.css file!
   app.use(express.static("client/build"));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
   const path = require("path");
-  app.get(
-    "*",
-    (req,
-    res) => {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    })
-  );
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 const PORT = process.env.PORT || 5000;
