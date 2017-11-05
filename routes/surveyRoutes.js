8784369ba85326edea4f7a37e698f81d577b21e4
edwin ismail <typhoon_ed@yahoo.com>
2017-11-05 12:44:08 +0300

    create back-end mailer and front-end forms
    add a mailer component in back end and imporve the front-end pages like creation of new survey.

---
+++ routes/surveyRoutes.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const requireLogin = require("../middleware/requireLogin");
+const requireCredits = require("../middleware/requireCredits");
+const Survey = mongoose.model("surveys");
+const Mailer = require("../services/Mailer");
+const surveyTemplate = require("../services/emailyTemplates/surveyTemplates");
+module.exports = app => {
+  app.get("/api/surveys/thanks", (req, res) => {
+    res.send("Thanks for voting!");
+  });
+  app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(",").map(email => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now()
+    });
+
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+
+    try {
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+
+      res.send(user);
+    } catch (err) {
+      res.status(422);
+    }
+  });
+};
